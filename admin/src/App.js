e9b427e026b981062df086f28807b4b3e545fc6f
Merabi-Giorgadze <124910119+Merabi-Giorgadze@users.noreply.github.com>
2024-10-27 21:22:11 +0400

    Update App.js
    edit names

--- admin/src/App.js
+++ admin/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './pages/home';
+import AnimalList from './pages/animalList';
 import AddAnimals from './pages/addanimals';
 import Categories from './pages/Categories';
 import CategoryList from './pages/CategoryList';
@@ -10,10 +10,10 @@ const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/add-animals" element={<AddAnimals />} />
-        <Route path="/categories" element={<Categories />} />
+        <Route path="/" element={<AnimalList />} />
+        <Route path="/add-animals" element={<AddAnimals />} />        
         <Route path="/categorieslist" element={<CategoryList />} />
+        <Route path="/categories" element={<Categories />} />
         <Route path="/CategAndAnim" element={<CategAndAnim />} />
       </Routes>
     </Router>
