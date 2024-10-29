60aeee184130593c9d9073cf1831f4bbd2b171c3
Merabi-Giorgadze <124910119+Merabi-Giorgadze@users.noreply.github.com>
2024-10-30 03:57:22 +0400

    Create actions.js
    actions

---
+++ client/src/redux/actions.js
@@ -0,0 +1,18 @@
+export const ADD_ANIMAL = 'ADD_ANIMAL';
+export const ADD_TO_WISHLIST = 'ADD_TO_WISHLIST';
+export const REMOVE_ANIMAL = 'REMOVE_ANIMAL';
+
+export const addAnimal = (animal) => ({
+  type: ADD_ANIMAL,
+  payload: animal,
+});
+
+export const addToWishlist = (animal) => ({
+  type: ADD_TO_WISHLIST,
+  payload: animal,
+});
+
+export const removeAnimal = (animalId) => ({
+  type: REMOVE_ANIMAL,
+  payload: animalId,
+});
