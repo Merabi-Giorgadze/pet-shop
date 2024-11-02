348ab4ea60207e1e2086ef792ed39332a6715711
Merabi-Giorgadze <124910119+Merabi-Giorgadze@users.noreply.github.com>
2024-11-03 00:44:26 +0400

    Update store.js
    cls commit

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,6 +1,6 @@
 import { createStore } from 'redux';
-import rootReducer from './rootReducer'; // Combine reducers
+import rootReducer from './rootReducer';
 
-const store = createStore(rootReducer); // Create store with rootReducer
+const store = createStore(rootReducer);
 
 export default store;
