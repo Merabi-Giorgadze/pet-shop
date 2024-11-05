03fb536ca1ac9b25ce0943eeff206642e4495079
Merabi-Giorgadze <124910119+Merabi-Giorgadze@users.noreply.github.com>
2024-11-05 20:10:13 +0400

    Update actions.js
    add counter

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,6 +1,7 @@
 export const ADD_ANIMAL = 'ADD_ANIMAL';
 export const ADD_TO_WISHLIST = 'ADD_TO_WISHLIST';
 export const REMOVE_ANIMAL = 'REMOVE_ANIMAL';
+export const REMOVE_FROM_WISHLIST = 'REMOVE_FROM_WISHLIST';
 
 export const addAnimal = (animal) => ({
   type: ADD_ANIMAL,
@@ -14,5 +15,10 @@ export const addToWishlist = (animal) => ({
 
 export const removeAnimal = (animalId) => ({
   type: REMOVE_ANIMAL,
+  payload: animalId,
+});
+
+export const removeFromWishlist = (animalId) => ({
+  type: REMOVE_FROM_WISHLIST,
   payload: animalId,
 });
