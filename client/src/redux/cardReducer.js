3ae7e726876c8f3741fa5e8e1786b9c7d9620708
Merabi-Giorgadze <124910119+Merabi-Giorgadze@users.noreply.github.com>
2024-11-05 20:09:12 +0400

    Update cardReducer.js
    add counter

--- client/src/redux/cardReducer.js
+++ client/src/redux/cardReducer.js
@@ -1,16 +1,31 @@
-import { ADD_TO_WISHLIST } from './actions';
+import { ADD_TO_WISHLIST, REMOVE_FROM_WISHLIST } from './actions';
 
 const initialState = {
-  wishlist: [],
+  wishlist: JSON.parse(localStorage.getItem('wishlist')) || [],
 };
 
 const cardReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_TO_WISHLIST:
+      const exists = state.wishlist.some(item => item._uuid === action.payload._uuid);
+      if (exists) {
+        return state;
+      }
+      const updatedWishlistAfterAdd = [...state.wishlist, action.payload];
+      localStorage.setItem('wishlist', JSON.stringify(updatedWishlistAfterAdd));
       return {
         ...state,
-        wishlist: [...state.wishlist, action.payload],
+        wishlist: updatedWishlistAfterAdd,
       };
+
+    case REMOVE_FROM_WISHLIST:
+      const updatedWishlistAfterRemove = state.wishlist.filter(item => item._uuid !== action.payload);
+      localStorage.setItem('wishlist', JSON.stringify(updatedWishlistAfterRemove));
+      return {
+        ...state,
+        wishlist: updatedWishlistAfterRemove,
+      };
+
     default:
       return state;
   }
